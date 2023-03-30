516bf4dcb58899241d8510a6a783c59c58bbab83
Kyle Ranous <kranous05@gmail.com>
2023-03-30 15:07:24 -0600

    Adjusted variable names to align to map_key and address throughout the entire package

--- mapping.py
+++ mapping.py
@@ -34,87 +34,87 @@ LOG_LEVEL = logging.DEBUG if LOG_LEVEL_SET.lower() in ['debug'] else logging.INF
 LOGGER.setLevel(LOG_LEVEL)
 
 
-def _get_static_string(value_targets):
+def _get_static_string(address):
     """
     Return the static string in Value
     """
-    LOGGER.debug('Mapping Static String: %s', value_targets)
-    if len(value_targets) > 1:
-        mapped_value = str(value_targets[1])
+    LOGGER.debug('Mapping Static String: %s', address)
+    if len(address) > 1:
+        mapped_value = str(address[1])
         return mapped_value
 
-    raise ValueError(f"Error converting value: {value_targets[1]} to string")
+    raise ValueError(f"Error converting value: {address[1]} to string")
 
-def _get_static_int(value_targets, **value_dicts):
+def _get_static_int(address, **value_dicts):
     """
     Takes in the value map and returns a static Integer
     """
-    LOGGER.debug('Mapping Static Integer: %s', value_targets)
-    if len(value_targets) > 1:
+    LOGGER.debug('Mapping Static Integer: %s', address)
+    if len(address) > 1:
         try:
-            mapped_value = int(value_targets[1])
+            mapped_value = int(address[1])
             return mapped_value
 
         except ValueError as exc:
             # Check if Statement has an OR clause
-            if len(value_targets) > 3:
-                if 'OR' in value_targets:
-                    new_target = ".".join(value_targets[value_targets.index('OR')+1:])
+            if len(address) > 3:
+                if 'OR' in address:
+                    new_address = ".".join(address[address.index('OR')+1:])
                     LOGGER.warning('Failed to Map Integer: %s, using OR clause: %s',
-                                   value_targets[1],
-                                   new_target)
+                                   address[1],
+                                   new_address)
 
-                    return _get_mapped_value(new_target, **value_dicts)
+                    return _get_mapped_value(new_address, **value_dicts)
 
-            LOGGER.error('Error converting value: %s to integer', value_targets[1])
-            raise ValueError(f"Error converting value: {value_targets[1]} to integer") from exc
+            LOGGER.error('Error converting value: %s to integer', address[1])
+            raise ValueError(f"Error converting value: {address[1]} to integer") from exc
 
-    raise RuntimeError(f"Error converting value: {value_targets[1]} to integer")
+    raise RuntimeError(f"Error converting value: {address[1]} to integer")
 
-def _get_static_float(value_targets, **value_dicts):
+def _get_static_float(address, **value_dicts):
     """
     Takes in the value map and returns a static Float value
     """
-    LOGGER.debug('Mapping Static Float: %s', value_targets)
+    LOGGER.debug('Mapping Static Float: %s', address)
     try:
-        if len(value_targets) > 1:
-            if value_targets[1].isdigit():
-                if len(value_targets) >= 3:
+        if len(address) > 1:
+            if address[1].isdigit():
+                if len(address) >= 3:
 
-                    if value_targets[2].isdigit():
+                    if address[2].isdigit():
 
-                        mapped_value = float(f'{value_targets[1]}.{value_targets[2]}')
+                        mapped_value = float(f'{address[1]}.{address[2]}')
                         return mapped_value
-                mapped_value = float(value_targets[1])
+                mapped_value = float(address[1])
                 return mapped_value
 
-        raise ValueError(f"Error converting value: {value_targets[1]} to float")
+        raise ValueError(f"Error converting value: {address[1]} to float")
     except ValueError as exc:
-        if len(value_targets) >= 3:
-            if 'OR' in value_targets:
-                new_target = ".".join(value_targets[value_targets.index('OR')+1:])
+        if len(address) >= 3:
+            if 'OR' in address:
+                new_address = ".".join(address[address.index('OR')+1:])
                 LOGGER.warning('Failed to Map Float: %s, using OR clause: %s',
-                               value_targets[1],
-                               new_target)
-                return _get_mapped_value(new_target, **value_dicts)
-        LOGGER.error('Error converting value: %s to float', value_targets[1])
-        raise ValueError(f"Error converting value: {value_targets[1]} to float") from exc
+                               address[1],
+                               new_address)
+                return _get_mapped_value(new_address, **value_dicts)
+        LOGGER.error('Error converting value: %s to float', address[1])
+        raise ValueError(f"Error converting value: {address[1]} to float") from exc
 
 
-def _get_static_list(value_targets):
+def _get_static_list(address):
     """
     Takes in the value_targets and returns a list
     """
-    LOGGER.debug('Mapping Static List: %s', value_targets)
-    if len(value_targets) > 1:
+    LOGGER.debug('Mapping Static List: %s', address)
+    if len(address) > 1:
         try:
-            value_targets[1] = value_targets[1].replace(', ', ',')
-            value_targets[1] = value_targets[1].replace(' ,', ',')
-            mapped_value = value_targets[1].split(',')
+            address[1] = address[1].replace(', ', ',')
+            address[1] = address[1].replace(' ,', ',')
+            mapped_value = address[1].split(',')
             return mapped_value
         except Exception as exc:
-            LOGGER.error('Error converting value: %s to list', value_targets[1])
-            raise RuntimeError(f"Error converting value: {value_targets[1]} to list") from exc
+            LOGGER.error('Error converting value: %s to list', address[1])
+            raise RuntimeError(f"Error converting value: {address[1]} to list") from exc
 
     return []
 
@@ -154,76 +154,76 @@ def map_dictionary(map_dict, **value_dicts):
     # Initialize the mapped dictionary
     mapped_dict = {}
     # Validate each key in the map dictionary and route to the appropriate function
-    for key, value in map_dict.items():
+    for map_key, address in map_dict.items():
         # If the value is SKIP, skip that key
-        LOGGER.debug('Mapping Key: %s to value: %s', key, value)
-        if value == 'SKIP':
-            LOGGER.debug('Skipping Key: %s', key)
+        LOGGER.debug('Mapping Key: %s to value: %s', map_key, address)
+        if address == 'SKIP':
+            LOGGER.debug('Skipping Key: %s', map_key)
             continue
 
         # If the value is a dictionary, pass the dictionary back to map_dictionary function
         # With the current value_dicts
-        if isinstance(value, dict):
-            LOGGER.info('Value is a dictionary, mapping dictionary to Key: %s', key)
-            result = map_dictionary(value, **value_dicts)
-            mapped_dict[key] = result
+        if isinstance(address, dict):
+            LOGGER.info('Value is a dictionary, mapping dictionary to Key: %s', map_key)
+            result = map_dictionary(address, **value_dicts)
+            mapped_dict[map_key] = result
         # Get the mapped value for the key
         else:
             #Check if result was skipped as part of follow on mapping
-            result = _get_mapped_value(value, **value_dicts)
+            result = _get_mapped_value(address, **value_dicts)
             if result == 'SKIP':
-                LOGGER.debug('Skipping Key: %s', key)
+                LOGGER.debug('Skipping Key: %s', map_key)
                 continue
-            mapped_dict[key] = result
+            mapped_dict[map_key] = result
     return mapped_dict
 
 
-def _get_mapped_value(map_address, **value_dicts):
+def _get_mapped_value(address, **value_dicts):
     """
     Takes in a map address and returns the value from the value dictionaries or static definition
     """
-    LOGGER.debug('Getting Mapped Value for: %s', map_address)
+    LOGGER.debug('Getting Mapped Value for: %s', address)
     # Split map_address into value keys
-    value_keys = map_address.split('.')
+    address_keys = address.split('.')
     # Check if the first key is `NULL` and return None if it is
-    if value_keys[0] == 'NULL':
+    if address_keys[0] == 'NULL':
         LOGGER.debug('Value is NULL, returning None')
         return None
     # Check for SKIP and return SKIP if found
-    if value_keys[0] == 'SKIP':
+    if address_keys[0] == 'SKIP':
         return 'SKIP'
         
     # Check if the first key is a reserved keyword
-    if value_keys[0] in RESERVED_KEYWORDS:
+    if address_keys[0] in RESERVED_KEYWORDS:
         try:
             # Call the reserved keyword function and return the result
             # Note Recursion and recalculation of value_keys is handled
             # in the reserved keyword functions
-            result = RESERVED_KEYWORDS[value_keys[0]](value_keys)
+            result = RESERVED_KEYWORDS[address_keys[0]](address_keys)
             return result
         except Exception as exc:
             # If the reserved keyword function fails, raise an exception
-            raise RuntimeError(f'Error getting mapped value: {map_address}') from exc
+            raise RuntimeError(f'Error getting mapped value: {address}') from exc
     else:
         # If the first key is not a reserved keyword, get the value for the map address
-        return _get_value_from_value_dict(map_address, **value_dicts)
+        return _get_value_from_value_dict(address, **value_dicts)
 
 
-def _get_value_from_value_dict(map_address, **value_dicts):
+def _get_value_from_value_dict(address, **value_dicts):
     """
     Takes in an address from the map and returns the value at that address from the value
     dictionaries.
     """
     # Split map_address into key_groups separated by OR keyword
-    key_groups = map_address.split('.OR.')
+    key_groups = address.split('.OR.')
 
     try:
         #Split Address from first key_group
-        address = key_groups[0].split('.')
+        address_keys = key_groups[0].split('.')
         # Get the first value from the value dictionaries
-        result = value_dicts[address[0]]
+        result = value_dicts[address_keys[0]]
         # Loop through the rest of the address keys and return the value in result
-        for address_key in address[1:]:
+        for address_key in address_keys[1:]:
             LOGGER.debug('Getting Value for Key: %s', address_key)
             result = result[address_key]
         # If a result was found, return it
@@ -238,5 +238,5 @@ def _get_value_from_value_dict(map_address, **value_dicts):
             # Rejoin the rest of the key_groups and send string back to _get_mapped_value
             return _get_mapped_value(new_target, **value_dicts)
         # If no more key_groups exist, raise an exception
-        LOGGER.error('Failed to map value from value dictionary: %s', map_address)
-        raise KeyError(f'Error getting value from value dictionary: {map_address}') from exc
+        LOGGER.error('Failed to map value from value dictionary: %s', address)
+        raise KeyError(f'Error getting value from value dictionary: {address}') from exc
